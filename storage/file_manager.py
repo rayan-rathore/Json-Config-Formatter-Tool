3e46a97e6a206f06aaaac6145ad229f6129bb2f0
rayan <rayanwissen1523@gmail.com>
2026-07-19 07:22:40 +0530

    added output function and intelligent handling of saving function to Back up file

--- storage/file_manager.py
+++ storage/file_manager.py
@@ -2,25 +2,49 @@
 """This dedicated file manager module is responsible for writing strings to disk,
 safely mutating user files, and handling safety duplications. """
 from pathlib import Path
-import os
 import shutil
 
 class StorageManager:
     def __init__(self):
         pass
 
-    def create_backup(self, filepath):
+    #---Action 1: create backup---
+    def create_backup_file(self, filepath):
         target_path = Path(filepath)
-        if target_path.exists() and filepath.is_file():
-            backup_path = target_path.with_name(target_path.name + ".back")
+
+        if target_path.exists() and target_path.is_file():
+            backup_path = target_path.with_name(target_path.name + ".bak")
             try:
                 shutil.copy2(target_path,backup_path)
-                return f"Backup created successfully: {backup_path.name}"
+                print( f"[INFO] Backup created successfully at {backup_path.name}")
             except IOError as e:
-                return f"ERROR: Could not create backup file. {e}"
+                print( f"[ERROR]: Could not create backup file. {e}")
         else:
-            return f"Error: The file '{target_path}' does not exist."
+            print( f"[ERROR]: The file '{target_path}' does not exist.")
+
+    #---Action 2: write output file---
+    def write_to_file(self,target_path,content):
+        with open(target_path, "w", encoding="utf-8") as file:
+            file.write(content)
+        print(f"[SUCCESS] Formatted JSON saved to '{target_path}.")
+
+    #---Action 3: handle intelligent mutation---
+    def save_result(self, original_path, output_path, in_place, content):
+        #if "output_path" is provided by user: save one on output by using action 2
+        if output_path is not None:
+            if in_place is True:
+                print(f"[INFO] Both --output and --in-place provided. Saving safely to separate path ")
+                self.write_to_file(output_path,content)
+
+        #In-place only if no safe separate output path was requested
+        elif in_place is True:
+            self.create_backup_file(original_path) # Auto-backup before overwrite for safety!
+            self.write_to_file(original_path,content)
+
+        #else: simply print content on the screen
+        else:
+            print(content)
 
 
-    def output_file(self):
-        
+
+
