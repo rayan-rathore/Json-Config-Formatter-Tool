f8b0c7d3226057c3237819ecc5b19bee8eba35f7
rayan <rayanwissen1523@gmail.com>
2026-07-19 20:11:41 +0530

    Add class JSONTreeGenerator

--- analyzer/tree_view.py
+++ analyzer/tree_view.py
@@ -1,1 +1,19 @@
 # Generates the visual terminal branch tree structural view
+"""
+├──  (Middle Branch)
+│    (Vertical Connector Space Guide)
+└──  (Final Corner Branch)
+"""
+
+class JSONTreeGenerator:
+
+    def generate_tree(self, data):
+        print(".")
+        self._paint_node(data,"")
+
+
+    def _paint_node(self,current_node, prefix):
+        if isinstance(current_node,dict):
+            pairs_list = list(current_node.items())
+            
+
